18b734b52f0e30f5c9fef7b6da829630e425a5c6
Farahani Mahendo <farahani.mahendo@rwjbh.org>
2022-06-09 10:14:37 -0400

    hfajdfkasd

--- src/sw.js
+++ src/sw.js
@@ -1,19 +1,15 @@
 document.body.addEventListener('push', function(event) {
-
   console.log('Received a push message', event);
-
-  function showNotification() {
-    Notification.requestPermission(function(result) {
-      if (result === 'granted') {
-        navigator.serviceWorker.ready.then(function(registration) {
-          registration.showNotification('Vibration Sample', {
-            body: 'Buzz! Buzz!',
-            icon: '../images/touch/chrome-touch-icon-192x192.png',
-            vibrate: [200, 100, 200, 100, 200, 100, 200],
-            tag: 'vibration-sample'
-          });
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          icon: '../images/touch/chrome-touch-icon-192x192.png',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: 'vibration-sample'
         });
-      }
-    });
-  }
+      });
+    }
+  });
 });
