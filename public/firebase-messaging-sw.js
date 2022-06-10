28db6e7e1ebf0e7d08f2ee39c5458585ccf4b10c
Farahani Mahendo <fmahendo@gmail.com>
2022-06-10 13:57:20 -0400

    fix some stuff

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,19 +1,24 @@
+// eslint-disable-next-line no-undef
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+// eslint-disable-next-line no-undef
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJV4J1F68K20_Ulw3MkLr1VGMuilq9tik",
-  authDomain: "esystemspush.firebaseapp.com",
-  projectId: "esystemspush",
-  storageBucket: "esystemspush.appspot.com",
-  messagingSenderId: "319177733947",
-  appId: "1:319177733947:web:a25577f3bc46529935291e"
+  apiKey: "AIzaSyDqYwENWxgEw2FwoUy4wDiAadNLEJiZt1o",
+  authDomain: "my-app-48628.firebaseapp.com",
+  projectId: "my-app-48628",
+  storageBucket: "my-app-48628.appspot.com",
+  messagingSenderId: "813348264268",
+  appId: "1:813348264268:web:f1425e8b5a166546fb17e9",
+  measurementId: "G-QE57ELW81K"
 };
 
 // Initialize Firebase
+// eslint-disable-next-line no-undef
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
+// eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
@@ -24,6 +29,7 @@ messaging.onBackgroundMessage(function(payload) {
     body: payload.notification.body,
   };
 
+  // eslint-disable-next-line no-restricted-globals
   self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
